3ff4bc449b853bf44c6b9df9f2086fb30a6aa9ae
Hugh 2098 <huanghuiqiang86@gmail.com>
2022-02-17 18:50:17 +0800

    feat: test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
-
+import React from 'react';
+import Demo from './Demo';
+// import Designable from './Designable';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'antd/dist/antd.css';
+import Log from './Log';
 function App() {
   return (
     <div>
       hello,world
+      <Demo/>
+      <Log/>
     </div>
   );
 }
